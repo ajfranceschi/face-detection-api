b7a04857840b1964ccd626b3029aea55b578ce4e
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-06-25 00:48:27 -0400

    moved image route code to image.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const knex = require('knex');
 const register = require('./controllers/register');
 const login = require('./controllers/login');
 const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 const saltRounds = 10;
 
 const database = knex({
@@ -70,20 +71,7 @@ app.get('/profile/:id', (req, res) => {
 
 // ####### IMAGE: INCREMENT ENTRIES ##########
 app.put('/image', (req, res) => {
-    const {id} = req.body;
-
-    return database('users')
-        .where('id', '=', id)
-        .returning('entries')
-        .increment('entries', 1)
-        .then(entries => {
-            if (entries.length > 0) {
-                res.json(entries[0]);
-            } else {
-                res.status(400).json("couldn't find user");
-            }
-        })
-        .catch(error => res.status(400).json('could not find user'));
+    image.handleImage(req, res, database);
 });
 
 app.listen(3000, () => {
