8d156416f8f31b02ec50fd8644089bcbdc989098
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-06-25 01:10:15 -0400

    added email, password, name is not empty validation

--- controllers/register.js
+++ controllers/register.js
@@ -1,34 +1,39 @@
 const saltRounds = 10;
 
 const handleRegister = (req, res, database, bcrypt) => {
-        const {name, email, password} = req.body;
+    const {name, email, password} = req.body;
 
-        // convert password to hash
-        const salt = bcrypt.genSaltSync(saltRounds);
-        const hash = bcrypt.hashSync(password, salt);
+    // Validation:
+    if (!email || !password || !name) {
+        return res.status(400).json('Incorrect form submission');
+    }
 
-        // insert into login and users using transaction
-        database.transaction(trx => {
-            trx('login')
-                .insert({
-                    email: email,
-                    hash: hash
-                })
-                .returning('email')
-                .then(loginEmail => {
-                    return trx('users')
-                        .returning('*')
-                        .insert({
-                            name: name,
-                            email: loginEmail[0],
-                            joined: new Date()
-                        })
-                        .then(user => res.json(user[0]))
-                })
-                .then(trx.commit)
-                .catch(trx.rollback);
-        }).catch(error => res.status(400).json('Error registering user'));
-    };
+    // convert password to hash
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+
+    // insert into login and users using transaction
+    database.transaction(trx => {
+        trx('login')
+            .insert({
+                email: email,
+                hash: hash
+            })
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        name: name,
+                        email: loginEmail[0],
+                        joined: new Date()
+                    })
+                    .then(user => res.json(user[0]))
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+    }).catch(error => res.status(400).json('Error registering user'));
+};
 
 module.exports = {
     handleRegister: handleRegister
